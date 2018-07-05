4b8f95f742e0fbf360ae5660297e40d26febad33
Adam Charytoniuk <adam.charytoniuk@gmail.com>
2018-07-05 19:51:29 +0100

    version that links with C++ library

--- build.rs
+++ build.rs
@@ -1,14 +1,39 @@
 extern crate cmake;
+
 use cmake::Config;
+use std::env;
 
 fn main()
 {
-    let dst = Config::new("libfoo") // The `libfoo` str here stands for the name of a source 
-                                    // directory with native code equipped with CMake driven build.
-                    .build();       // This runs the cmake to generate makefiles and build it all eventually
+    let dst = Config::new("libfoo")    // The `libfoo` str here stands for the name of a source 
+                                        // directory with native code equipped with CMake driven build.
+                   .build();            // This runs the cmake to generate makefiles and build it all eventually
 
-    // now - emitting some cargo commands to build and link the lib
+    // Same for C++ - to build the static libary from libfoo++ subdirectory,
+    // but we don't need to consume its result. 
+    Config::new("libfoo++").build();      
+
+    // Now - emitting some cargo commands to build and link the lib. 
+    // This turns to be common to both our libs, so we do it once.
     println!("cargo:rustc-link-search=native={}", dst.display());
     // Phase `foo` here stands for the library name (without lib prefix and without .a suffix)
     println!("cargo:rustc-link-lib=static=foo");    
+    
+
+    println!("cargo:rustc-link-lib=static=foo++");
+
+    // C++ is bit more complicated, since platform specifics come to play
+    let target  = env::var("TARGET").unwrap();
+    if target.contains("apple")
+    {
+        println!("cargo:rustc-link-lib=dylib=c++");
+    }
+    else if target.contains("linux")
+    {
+        println!("cargo:rustc-link-lib=dylib=stdc++");
+    }
+    else 
+    {
+        unimplemented!();
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
-
-// this tells we're declaring something living in the external library
-// `foo` stands here as a name of the library (wihout lib prefix)
-#[link(name="foo", kind="static")]
 extern { 
+    // this tells we're declaring something living in the external library
+    // `foo` and `foo++` stand here as names of the libraries (wihout lib prefix)
+
     // this is rustified prototype of the function from our C library
+    #[link(name="foo", kind="static")]
     fn testcall(v: f32); 
+
+    // this is rustified prototype of the function from our C++ library
+    #[link(name="foo++", kind="static")]
+    fn testcall_cpp(v: f32); 
 }
 
 fn main() {
@@ -14,5 +18,6 @@ fn main() {
     // In rust this comes via unsafe code block.
     unsafe { 
         testcall(3.14159); 
+        testcall_cpp(3.14159); 
     };
 }
